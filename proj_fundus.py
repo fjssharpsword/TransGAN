78c419b5c66ae608ebe20c3037ebf7fa02c0b372
fjssharpsword <sharpsword@163.com>
2021-12-20 00:56:08 +0000

    1220

--- proj_fundus.py
+++ proj_fundus.py
@@ -5,23 +5,23 @@ import sys
 import torch
 from torch import optim
 from torch.nn import functional as F
-from torchvision import transforms
+from torchvision import transforms, utils
 from PIL import Image
 from tqdm import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
 import heapq
 import numpy as np
-import lpips
+import nets.lpips as lpips
 from datasets.fundus_idrid_grading import get_train_dataset_fundus, get_test_dataset_fundus
 from nets.model import Generator, Discriminator, TransEnc, CNNEnc
+from nets.perceptual import VGGPerceptualLoss
 
-if __name__ == "__main__":
-    
+def retrivel_per():    
     #load model
     device = "cuda:7"
     transenc = TransEnc(256, 256).to(device)
     cnnsenc = CNNEnc(256, 256).to(device)
-    ckpt_path = "/data/pycode/TransGAN/ckpts/090000.pt"
+    ckpt_path = "/data/pycode/TransGAN/ckpts/149999.pt"
     transenc.load_state_dict(torch.load(ckpt_path, map_location={'cuda:0': 'cuda:7'})["trans"], strict=False)
     transenc.eval()
     cnnsenc.load_state_dict(torch.load(ckpt_path, map_location={'cuda:0': 'cuda:7'})["cnns"], strict=False)
@@ -31,7 +31,6 @@ if __name__ == "__main__":
     train_loader = torch.utils.data.DataLoader(get_train_dataset_fundus(), batch_size=16, shuffle=True,num_workers=0, pin_memory=True)
     test_loader = torch.utils.data.DataLoader(get_test_dataset_fundus(), batch_size=16, shuffle=False, num_workers=0, pin_memory=True)
 
-
     #retrieval evaluation
     print('********************Build feature for trainset!********************')
     tr_label = torch.FloatTensor()
@@ -40,9 +39,9 @@ if __name__ == "__main__":
         for batch_idx, (image, label) in enumerate(train_loader):
             tr_label = torch.cat((tr_label, label), 0)
 
-            #latent_in_trans, latent_in_cnn = transenc(image.to(device)),  cnnsenc(image.to(device))
-            #var_feat = torch.cat((latent_in_trans, latent_in_cnn), dim=-1)
-            var_feat = cnnsenc(image.to(device))
+            latent_in_trans, latent_in_cnn = transenc(image.to(device)),  cnnsenc(image.to(device))
+            var_feat = torch.cat((latent_in_trans, latent_in_cnn), dim=-1)
+            #var_feat = cnnsenc(image.to(device))
 
             tr_feat = torch.cat((tr_feat, var_feat.cpu().data.view(var_feat.shape[0],-1)), 0)
             sys.stdout.write('\r train set process: = {}'.format(batch_idx + 1))
@@ -54,9 +53,9 @@ if __name__ == "__main__":
         for batch_idx, (image, label) in enumerate(test_loader):
             te_label = torch.cat((te_label, label), 0)
 
-            #latent_in_trans, latent_in_cnn = transenc(image.to(device)),  cnnsenc(image.to(device))
-            #var_feat = torch.cat((latent_in_trans, latent_in_cnn), dim=-1)
-            var_feat = cnnsenc(image.to(device))
+            latent_in_trans, latent_in_cnn = transenc(image.to(device)),  cnnsenc(image.to(device))
+            var_feat = torch.cat((latent_in_trans, latent_in_cnn), dim=-1)
+            #var_feat = cnnsenc(image.to(device))
 
             te_feat = torch.cat((te_feat, var_feat.cpu().data.view(var_feat.shape[0],-1)), 0)
             sys.stdout.write('\r test set process: = {}'.format(batch_idx + 1))
@@ -107,4 +106,135 @@ if __name__ == "__main__":
             print('Fundus mAP of {} is {:.4f}'.format(CLASS_NAMES[i], np.mean(mAPs[i])))
         print("Fundus Average mAP@{}={:.4f}".format(topk, np.mean(mAPs_avg)))
 
+
+def get_lr(t, initial_lr, rampdown=0.25, rampup=0.05):
+    lr_ramp = min(1, (1 - t) / rampdown)
+    lr_ramp = 0.5 - 0.5 * math.cos(lr_ramp * math.pi)
+    lr_ramp = lr_ramp * min(1, t / rampup)
+
+    return initial_lr * lr_ramp
+
+def recover_fundus():
+    #load model
+    device = "cuda:6"
+    transenc = TransEnc(256, 256).to(device)
+    cnnsenc = CNNEnc(256, 256).to(device)
+    ckpt_path = "/data/pycode/TransGAN/ckpts/199999.pt"
+    transenc.load_state_dict(torch.load(ckpt_path, map_location={'cuda:0': 'cuda:6'})["trans"], strict=False)
+    transenc.eval()
+    cnnsenc.load_state_dict(torch.load(ckpt_path, map_location={'cuda:0': 'cuda:6'})["cnns"], strict=False)
+    cnnsenc.eval()
+    g_ema = Generator(256, 512, 8)
+    g_ema.load_state_dict(torch.load(ckpt_path, map_location={'cuda:0': 'cuda:6'})["g_ema"], strict=False)
+    g_ema.eval()
+    g_ema = g_ema.to(device)
+
+    #load dataset
+    train_loader = torch.utils.data.DataLoader(get_train_dataset_fundus(), batch_size=8, shuffle=True,num_workers=0, pin_memory=True)
+    with torch.autograd.no_grad():
+        for batch_idx, (imgs, lbls) in enumerate(train_loader):
+            imgs = imgs.to(device)
+            latent_in_trans, latent_in_cnn = transenc(imgs),  cnnsenc(imgs)
+            latent_in = torch.cat((latent_in_trans, latent_in_cnn), dim=-1)
+
+            with torch.autograd.enable_grad():
+                latent_in.requires_grad = True
+                optimizer = optim.Adam([latent_in], lr=0.1)
+                pbar = tqdm(range(100000))
+                latent_path = []
+                for i in pbar:
+                    t = i / 100000
+                    lr = get_lr(t, 0.1)
+                    optimizer.param_groups[0]["lr"] = lr
+            
+                    img_gen, _ = g_ema([latent_in], input_is_latent=True)
+                    mse_loss = F.mse_loss(img_gen, imgs)
+                    optimizer.zero_grad()
+                    mse_loss.backward()
+                    optimizer.step()
+                    if (i + 1) % 100 == 0:
+                        latent_path.append(latent_in.detach().clone())
+
+                    pbar.set_description((f"perceptual: {mse_loss.item():.4f}; lr: {lr:.4f}"))
+
+            img_gen, _ = g_ema([latent_path[-1]], input_is_latent=True)
+            utils.save_image(
+                            torch.cat((imgs.cpu().data, img_gen.cpu().data), 0),
+                            f"/data/pycode/TransGAN/logs/recov_fundus.png",
+                            nrow=8,
+                            normalize=True,
+                            range=(-1, 1),
+                        )
+            break
+
+def perceptual_evaluation():
+    #load model
+    device = "cuda:7"
+    transenc = TransEnc(256, 256).to(device)
+    cnnsenc = CNNEnc(256, 256).to(device)
+    ckpt_path = "/data/pycode/TransGAN/ckpts/149999.pt"
+    transenc.load_state_dict(torch.load(ckpt_path, map_location={'cuda:0': 'cuda:7'})["trans"], strict=False)
+    transenc.eval()
+    cnnsenc.load_state_dict(torch.load(ckpt_path, map_location={'cuda:0': 'cuda:7'})["cnns"], strict=False)
+    cnnsenc.eval()
+    vggptloss = VGGPerceptualLoss(matching_loss="mse").to(device)
+
+    #load dataset
+    train_loader = torch.utils.data.DataLoader(get_train_dataset_fundus(), batch_size=16, shuffle=True,num_workers=0, pin_memory=True)
+    test_loader = torch.utils.data.DataLoader(get_test_dataset_fundus(), batch_size=16, shuffle=False, num_workers=0, pin_memory=True)
+
+    #retrieval evaluation
+    print('********************Build feature for trainset!********************')
+    tr_image = torch.FloatTensor()
+    tr_feat = torch.FloatTensor()
+    with torch.autograd.no_grad():
+        for batch_idx, (image, label) in enumerate(train_loader):
+            tr_image = torch.cat((tr_image, image), 0)
+
+            latent_in_trans, latent_in_cnn = transenc(image.to(device)),  cnnsenc(image.to(device))
+            var_feat = torch.cat((latent_in_trans, latent_in_cnn), dim=-1)
+            #var_feat = cnnsenc(image.to(device))
+
+            tr_feat = torch.cat((tr_feat, var_feat.cpu().data.view(var_feat.shape[0],-1)), 0)
+            sys.stdout.write('\r train set process: = {}'.format(batch_idx + 1))
+            sys.stdout.flush()
+    print('********************Extract feature for testset!********************')
+    te_image = torch.FloatTensor()
+    te_feat = torch.FloatTensor()
+    with torch.autograd.no_grad():
+        for batch_idx, (image, label) in enumerate(test_loader):
+            te_image = torch.cat((te_image, image), 0)
+
+            latent_in_trans, latent_in_cnn = transenc(image.to(device)),  cnnsenc(image.to(device))
+            var_feat = torch.cat((latent_in_trans, latent_in_cnn), dim=-1)
+            #var_feat = cnnsenc(image.to(device))
+
+            te_feat = torch.cat((te_feat, var_feat.cpu().data.view(var_feat.shape[0],-1)), 0)
+            sys.stdout.write('\r test set process: = {}'.format(batch_idx + 1))
+            sys.stdout.flush()
+    print('********************Retrieval Performance!********************')
+    sim_mat = cosine_similarity(te_feat.numpy(), tr_feat.numpy())
+    tr_image = tr_image.to(device)
+    te_image = te_image.to(device)
+
+    for topk in [5, 10, 20]:
+        # NDCG: lack of ground truth ranking labels
+        perc_seq = []
+        for i in range(sim_mat.shape[0]):
+            idxs, vals = zip(*heapq.nlargest(topk, enumerate(sim_mat[i, :].tolist()), key=lambda x: x[1]))
+            query_img = te_image[i]
+            loss_seq = []
+            for j in idxs:
+                return_img = tr_image[j]
+                p_loss = vggptloss(query_img, return_img)
+                loss_seq.append(loss_seq)
+            perc_seq.append(np.mean(loss_seq))
+        # average perceptual
+        print("Fundus Average Perception@{}={:.4f}".format(topk, np.mean(perc_seq)))
+
+if __name__ == "__main__":
+
+    #retrivel_per()
+    recover_fundus()
+    #perceptual_evaluation()
     #python proj_fundus.py
